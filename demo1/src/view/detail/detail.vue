2f145ee9719dc07f271f8a729345c79b28e807c9
SunYuHang11 <2380815741@qq.com>
2023-08-16 15:46:42 +0800

    最新修改

---
+++ demo1/src/view/detail/detail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="#vue">
+    <div v-if="falg"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    el:"#vue",
+    data(){
+        return{
+            falg:true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

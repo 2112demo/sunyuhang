8b68b3af2ff0eb7ce38f7e5b51593aaaba9be95e
SunYuHang11 <2380815741@qq.com>
2023-08-16 15:22:15 +0800

    最新修改的vue项目

---
+++ demo1/src/view/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="index">
+    <h1>{{ msg }}</h1>
+    <button v-on-click="count++"></button>
+  </div>
+</template>
+
+<script>
+export default {
+    el:"#index",
+    data(){
+        return{
+            msg:"哈哈哈哈",
+            count:1
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
